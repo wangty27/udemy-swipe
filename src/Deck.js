468497d78ecb826a07a58b925687c1fb0f3ad8eb
Terry Wang <terrywang@tianyu.ninja>
2018-07-14 18:51:00 -0400

    Card Rotate

--- src/Deck.js
+++ src/Deck.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import {
   View,
   Animated,
-  PanResponder
+  PanResponder,
+  Dimensions
 } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
   constructor(props) {
@@ -15,10 +18,31 @@ class Deck extends Component {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       },
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: () => {
+        this.resetPosition();
+      }
     });
 
     this.state = { panResponder, position };
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
   }
 
   renderCards() {
@@ -28,7 +52,7 @@ class Deck extends Component {
           <Animated.View
             key={card.id}
             {...this.state.panResponder.panHandlers}
-            style={this.state.position.getLayout()}
+            style={this.getCardStyle()}
           >
             {this.props.renderCard(card)}
           </Animated.View>
